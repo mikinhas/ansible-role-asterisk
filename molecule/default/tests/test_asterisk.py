d9c20b67c4631fa9720b33eefeb4bb358a6282e6
Michael MACHADO <michael.machado.reino@gmail.com>
2024-11-12 22:15:15 +0100

    Init asterisk tests

---
+++ molecule/default/tests/test_asterisk.py
@@ -0,0 +1,24 @@
+
+import os
+import testinfra.utils.ansible_runner
+
+def test_asterisk_is_installed(host):
+    asterisk = host.package("asterisk")
+    assert asterisk.is_installed
+
+def test_asterisk_running_and_enabled(host):
+    asterisk = host.service("asterisk")
+    assert asterisk.is_running
+    assert asterisk.is_enabled
+
+def test_asterisk_config_exists(host):
+    config = host.file("/etc/asterisk/asterisk.conf")
+    assert config.exists
+    assert config.user == "asterisk"
+    assert config.group == "asterisk"
+    assert config.mode == 0o640
+
+    pjsip_conf = host.file("/etc/asterisk/pjsip.conf")
+    assert pjsip_conf.exists
+    assert pjsip_conf.contains("protocol=udp")
+    assert pjsip_conf.contains("bind=0.0.0.0")
